4e3e94a0d371d3eec91c991c27bb7be88470d132
Kusheel Sai <kusheelsaisanaboyina@gmail.com>
2026-03-10 19:24:49 +0530

    first commit

---
+++ src/test/java/com/company/course_api/CourseApiApplicationTests.java
@@ -0,0 +1,12 @@
+package com.company.course_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApiApplicationTests {
+    public static void main(String[] args) {
+        SpringApplication.run(CourseApiApplication.class, args);
+    }
+}
+
